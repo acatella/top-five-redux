fc928fe6ee0ca8f8a7623af74fb74a50bcbafa93
acatella <aficclark@gmail.com>
2016-05-12 19:24:49 -0700

    added cdn and comment properties to post object, put video, thumbnail, title, comment link into view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the topFiveReduxApp
  */
 angular.module('topFiveReduxApp')
-  .controller('MainCtrl', function ($scope, redditService) {
+  .controller('MainCtrl', function ($scope,$sce, redditService) {
 
     //right now this executes on load
     //wrap it in a function to have it trigged on click
@@ -17,15 +17,31 @@ angular.module('topFiveReduxApp')
       var posts = data.data.children;
       for (var i = 0; i < posts.length; i++) {
         if (!posts[i].data.domain.search('streamable')) {
-          gifs.push(posts[i]);
+          var post = posts[i];
+          gifs.push(post);
+
+          // gets unique ID for each streamable link
+          var urlArray = post.data.url.split('/');
+          var uniqueID = urlArray[urlArray.length-1];
+
           // add property for comments href to posts[i].data
-          // add property for cdn link to video to posts[i].data
+          post.commentLink = 'https://www.reddit.com' + post.data.permalink;
+
           // add property for cdn link to thumbnail to posts[i].data
+          post.thumbnail = 'https://cdn.streamable.com/image/' + uniqueID + '.jpg';
+
+          // add property for cdn link to thumbnail to posts[i].data
+          post.videoSource = 'https://cdn.streamable.com/video/mp4/' + uniqueID + '.mp4';
+
         }
       }
       $scope.gifs = gifs;
 
     });
+
+    $scope.trustSrc = function(src) {
+      return $sce.trustAsResourceUrl(src);
+    };
 
     $scope.reOrder = function(post, index) {
       var tmp = post;
